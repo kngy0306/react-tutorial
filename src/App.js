b683d751f30761650ca56bb0dffa4090fa81cb77
Kona <kitami.kitano20@gmail.com>
2021-09-16 09:06:20 +0900

    :sparkles: Contextを利用したテーマの変更

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import styled from "styled-components";
-import { getLanguages } from "./const/languages";
 import { Form } from "./Form";
-import { withLoading } from "./hoc/withLoading";
 import { List } from "./List";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Header } from "./Header";
+import styled from "styled-components";
+import { ThemeContext } from "./context/ThemeContext";
 
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  padding: 24px 64px 0;
-  border-bottom: 1px solid #e0e0e0;
-`;
-
-const HeaderUl = styled.ul`
-  display: flex;
-  padding: 0;
-  margin: 0;
-`;
-
-const HeaderLi = styled.li`
-  list-style: none;
-  padding: 4px 12px;
-  cursor: pointer;
-  border-bottom: ${(props) => (props.focused ? "2px solid #f44336" : "none")};
+const Container = styled.div`
+  height: 100%;
+  color: ${({ theme }) => theme.color};
+  background-color: ${({ theme }) => theme.backgroundColor};
 `;
 
 function App({ data }) {
   const [tab, setTab] = useState("list");
   const [langs, setLangs] = useState(data);
+
+  const [theme] = useContext(ThemeContext);
 
   const addLang = (lang) => {
     setLangs([...langs, lang]);
@@ -38,20 +26,11 @@ function App({ data }) {
     tab === "list" ? <List langs={langs} /> : <Form onAddLang={addLang} />;
 
   return (
-    <div>
-      <Header>
-        <HeaderUl>
-          <HeaderLi focused={tab === "list"} onClick={() => setTab("list")}>
-            リスト
-          </HeaderLi>
-          <HeaderLi focused={tab === "form"} onClick={() => setTab("form")}>
-            フォーム
-          </HeaderLi>
-        </HeaderUl>
-      </Header>
+    <Container theme={theme}>
+      <Header tab={tab} setTab={setTab} />
       {body}
-    </div>
+    </Container>
   );
 }
 
-export default withLoading(App, getLanguages);
+export default App;
