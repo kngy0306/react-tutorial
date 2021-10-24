1a2bdc94a2ed9c69150a4134b80ba5f50427756e
Kona <kitami.kitano20@gmail.com>
2021-10-24 09:51:40 +0900

    :memo: #9 useEffectのユースケース

--- trahack-tutorial/README.md
+++ trahack-tutorial/README.md
@@ -391,4 +391,51 @@ useEffect(() => {
 
 Unmount時に呼ばれる関数のことをクリーンアップ関数という。
 
-## #9
+## #9 useEffect内でAPIを叩く
+
+useEffectのユースケース
+
+- API、DBから非同期通信でデータを取得(fetch)する
+- 特定の値が変更したらデータを再取得(refetch)する
+
+### useEffect内で非同期処理通信
+
+useEffectでコンポーネントの生成時にapiを叩き、buttonをクリックするとidをランダムに取得して変更する。そのたびにuseEffectの第2引数にidを指定しているので値が変わるたびにuseEffect内の処理が走る。
+
+```js
+import { useEffect, useState } from "react";
+
+function App() {
+  const [id, setId] = useState("kngy0306");
+  const [name, setName] = useState("");
+  const ids = ["kngy0306", "google", "facebook"];
+
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)];
+    setId(_id);
+  };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.name);
+        setName(data.name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
+  return (
+    <>
+      <h3>
+        {name}のGitHubIDは{id}です
+      </h3>
+      <button onClick={getRandomId}>ランダムにidを取得</button>
+    </>
+  );
+}
+
+export default App;
+```
--- trahack-tutorial/src/App.js
+++ trahack-tutorial/src/App.js
@@ -1,26 +1,33 @@
-import Article from "./components/Article";
 import { useEffect, useState } from "react";
-import { Input } from "./components/Input";
-import { Counter } from "./components/Counter";
-import { ToggleButton } from "./components/ToggleButton";
 
 function App() {
-  const firstTitle = "最初のタイトル！";
-  const kona = "kona";
+  const [id, setId] = useState("kngy0306");
+  const [name, setName] = useState("");
+  const ids = ["kngy0306", "google", "facebook"];
 
-  const [state, setState] = useState(false);
-  const changeState = () => {
-    const forwardState = !state;
-    setState(forwardState);
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)];
+    setId(_id);
   };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.name);
+        setName(data.name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
 
   return (
     <>
-      <Article title={firstTitle} clickFunc={changeState} state={state} />
-      <Input />
-      <Counter />
-      <ToggleButton />
-      <h3>{kona}</h3>
+      <h3>
+        {name}のGitHubIDは{id}です
+      </h3>
+      <button onClick={getRandomId}>ランダムにidを取得</button>
     </>
   );
 }
